6aa0014e52200b7a775089b6ec630f1bbea2c817
Jose Quesada <jquesada2016@fau.edu>
2019-09-06 14:25:02 -0400

    Added my (Jose) about page.
    I essentially just copied and pasted Matt's about page as a temporary placeholder
    until we decide as a group if we want to have consistent personal pages,
    or unique and stylized ones.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import Matt from './about/matt/matt'
+import Jose from './about/jose/jose'
 
 const routing = (
     <Router>
         <div>
             <Route path exact="/" component={App} />
             <Route path="/matt" component={Matt} />
+            <Route path="/jose" component={Jose} />
         </div>
     </Router>
 )
